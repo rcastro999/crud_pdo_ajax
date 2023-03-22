8bf9722cef61c08e360dadf915c2283df69a02e9
desarrollador3agrosalva <desarrollador3agrosalva@gmail.com>
2023-03-22 16:48:47 -0600

    modificado

--- conexion.php
+++ conexion.php
@@ -1,5 +1,15 @@
 <?php
 
-    $usuario = "root";
-    $password = "";
-    $conexion = new PDO("mysql:host=localhost;dbname=crud_usuarios", $usuario, $password);
+
+    $dbHost = 'localhost';
+    $dbName = 'crud_usuarios';
+    $dbUser = 'root';
+    $dbPass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa";
+} catch(PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
