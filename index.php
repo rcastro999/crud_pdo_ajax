b01b244e26a966aa607438f20a0f5b2ac7d2f478
desarrollador3agrosalva <desarrollador3agrosalva@gmail.com>
2023-03-23 10:19:58 -0600

    estes es una adicion

--- index.php
+++ index.php
@@ -10,106 +10,109 @@
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-    <link rel="stylesheet" href="./css/estilos.css">
-    
-    <title>CRUD PHP PDO AJAX DATATABLES</title>
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <title>CRUD con PHP, PDO, Ajax y Datatables.js</title>
   </head>
   <body>
-    <div class="container fondo">
-      <h1 class="text-center">CRUD PHP PDO AJAX DATATABLES</h1>
-      <h1 class="text-center">Roland C.</h1>
 
-      <div class="row">
-        <div class="col-2 offset-10">
-          <div class="text-center">
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#modalUsuario" id="botonCrear">
-              <i class="bi bi-plus-circle"></i> Crear
-            </button>
-          </div>
+    <div class="container fondo">
+
+        <h1 class="text-center">CRUD con PHP, PDO, Ajax y Datatables.js</h1>
+        <h1 class="text-center">www.render2web.com</h1>
+
+        <div class="row">
+            <div class="col-2 offset-10">
+                <div class="text-center">
+                    <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#modalUsuario" id="botonCrear">
+                        <i class="bi bi-plus-circle-fill"></i> Crear
+                        </button>
+                </div>
+            </div>
         </div>
-      </div>
-        <br>
-        <br>
+        <br />
+        <br />
+
         <div class="table-responsive">
             <table id="datos_usuario" class="table table-bordered table-striped">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Nombre</th>
-                  <th>Apellidos</th>
-                  <th>Telefono</th>
-                  <th>Email</th>
-                  <th>Imagen</th>
-                  <th>Fecha Creacion</th>
-                  <th>Editar</th>
-                  <th>Borrar</th>
-                </tr>
-              </thead>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Apellidos</th>
+                        <th>Teléfono</th>
+                        <th>Email</th>
+                        <th>Imagen</th>
+                        <th>Fecha Creación</th>
+                        <th>Editar</th>
+                        <th>Borrar</th>
+                    </tr>
+                </thead>
             </table>
         </div>
-    </div>
+   </div>
 
 
-
-      <!-- Modal -->
-      <div class="modal fade" id="modalUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Crear Usuario</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          
-            <form action="" method="POST" id="formulario" enctype="multipart/form-data">
-                <div class="modal-content">
-                  <div class="modal-body">
+<!-- Modal -->
+<div class="modal fade" id="modalUsuario" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Crear Usuario</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+     
+        <form method="POST" id="formulario" enctype="multipart/form-data">
+            <div class="modal-content">
+                <div class="modal-body">
                     <label for="nombre">Ingrese el nombre</label>
                     <input type="text" name="nombre" id="nombre" class="form-control">
+                    <br />
 
                     <label for="apellidos">Ingrese los apellidos</label>
                     <input type="text" name="apellidos" id="apellidos" class="form-control">
+                    <br />
 
-                    <label for="telefono">Ingrese el Telefono</label>
+                    <label for="telefono">Ingrese el teléfono</label>
                     <input type="text" name="telefono" id="telefono" class="form-control">
-
+                    <br />
 
                     <label for="email">Ingrese el email</label>
-                    <input type="text" name="email" id="email" class="form-control">
+                    <input type="email" name="email" id="email" class="form-control">
+                    <br />
 
-                    <label for="nombre">Seleccione una imagen</label>
+                    <label for="imagen">Seleccione una imagen</label>
                     <input type="file" name="imagen_usuario" id="imagen_usuario" class="form-control">
                     <span id="imagen_subida"></span>
+                    <br />
                 </div>
 
                 <div class="modal-footer">
-                  <input type="hidden" name="id_usuario" id="id_usuario">
-                  <input type="hidden" name="operacion" id="operacion">
-                  <input type="submit" name="action" id="action" class="btn btn-success" value="Crear">
+                    <input type="hidden" name="id_usuario" id="id_usuario">
+                    <input type="hidden" name="operacion" id="operacion">             
+                    <input type="submit" name="action" id="action" class="btn btn-success" value="Crear">
                 </div>
-              </div>
-            </form>
-          </div>
-        </div>
-    </div>
+            </div>
+        </form>
+      </div>     
+  </div>
+</div>
 
-    <!-- J QUERY -->
+    
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-
-
     <script src="//cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-
 
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-
-  <script type="text/javascript">
+    <script type="text/javascript">
         $(document).ready(function(){
             $("#botonCrear").click(function(){
                 $("#formulario")[0].reset();
@@ -194,8 +197,59 @@
                     alert("Algunos campos son obligatorios");
                 }
 	        });
-  </script>
 
+            //Funcionalida de editar
+            $(document).on('click', '.editar', function(){		
+            var id_usuario = $(this).attr("id");		
+            $.ajax({
+                url:"obtener_registro.php",
+                method:"POST",
+                data:{id_usuario:id_usuario},
+                dataType:"json",
+                success:function(data)
+                    {
+                        //console.log(data);				
+                        $('#modalUsuario').modal('show');
+                        $('#nombre').val(data.nombre);
+                        $('#apellidos').val(data.apellidos);
+                        $('#telefono').val(data.telefono);
+                        $('#email').val(data.email);
+                        $('.modal-title').text("Editar Usuario");
+                        $('#id_usuario').val(id_usuario);
+                        $('#imagen_subida').html(data.imagen_usuario);
+                        $('#action').val("Editar");
+                        $('#operacion').val("Editar");
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                    console.log(textStatus, errorThrown);
+                    }
+                })
+	        });
 
+            //Funcionalida de borrar
+            $(document).on('click', '.borrar', function(){
+                var id_usuario = $(this).attr("id");
+                if(confirm("Esta seguro de borrar este registro:" + id_usuario))
+                {
+                    $.ajax({
+                        url:"borrar.php",
+                        method:"POST",
+                        data:{id_usuario:id_usuario},
+                        success:function(data)
+                        {
+                            alert(data);
+                            dataTable.ajax.reload();
+                        }
+                    });
+                }
+                else
+                {
+                    return false;	
+                }
+            });
+
+        });         
+    </script>
+    
   </body>
 </html>
